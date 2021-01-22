dfd518ff292113e668eece1f8d7dc5f93345ab52
ahmed-razic <ahmed.razic@gmail.com>
2021-01-22 21:44:37 +0100

    added eventListeners

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.android.miwok;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView numbers = findViewById(R.id.numbers);
+        numbers.setOnClickListener(view -> {
+            Intent numbersIntent = new Intent(MainActivity.this, NumbersActivity.class);
+            startActivity(numbersIntent);
+        });
+
+        TextView family = findViewById(R.id.family);
+        family.setOnClickListener(view -> {
+            Intent familyIntent = new Intent(MainActivity.this, FamilyActivity.class);
+            startActivity(familyIntent);
+        });
+
+        TextView phrases = findViewById(R.id.phrases);
+        phrases.setOnClickListener(view -> {
+            Intent phrasesIntent = new Intent(MainActivity.this, PhrasesActivity.class);
+            startActivity(phrasesIntent);
+        });
+
+        TextView colors = findViewById(R.id.colors);
+        colors.setOnClickListener(view -> {
+            Intent colorsIntent = new Intent(MainActivity.this, ColorsActivity.class);
+            startActivity(colorsIntent);
+        });
     }
+
+
+
+
+
+/*    public void openNumbersActivity(View view) {
+
+    }*/
+
+
 }
